8b754ae1d8a5f82315edca9934b5b8c46ec24990
Juel07 <juel.braganza97@gmail.com>
2021-06-21 01:21:57 +0100

    user can see days left until Bday

--- birthday-app/app.rb
+++ birthday-app/app.rb
@@ -1,3 +1,4 @@
+require "date"
 require "sinatra"
 require "sinatra/reloader" if development?
 
@@ -5,14 +6,26 @@ configure :production do
   enable :reloader
 end
 
-get "/" do # form
+get "/" do
   erb :index
 end
 
-post "/birthday" do # birthday greetings & countdown
-  puts params
+post "/birthday" do
+  puts params # {"name"=>"Lola", "day"=>"4", "month"=>"may"}
   @name = params[:name]
-  @day = params[:day]
-  @days_left = 8
+  @day = params[:day].to_i
+  @birth_month = params[:month].capitalize!
+  month = Date::ABBR_MONTHNAMES.index(@birth_month) # Date::MONTHNAMES.index("Jun") => 6
+  year = Time.now.year
+  birth_day = Date.new(year, month, @day)
+  today = Date.today
+
+  if birth_day > today
+    @days_left = (birth_day - today).to_i
+  else
+    @days_left = (birth_day.next_year - today).to_i
+  end
+
+  @plural = "s" if (@days_left > 1)
   erb :user_birthday
 end
